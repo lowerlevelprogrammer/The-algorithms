9d986ba4af321daeac033b5f51b3e9f2c6c6e184
Sumeet Mathpati <sumeet.mathpati@gmail.com>
2020-02-16 21:50:20 +0530

    addes array in sorting pages

--- lib/ui/Sort.dart
+++ lib/ui/Sort.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Sort extends StatefulWidget {
   @override
@@ -6,10 +7,25 @@ class Sort extends StatefulWidget {
 }
 
 class _Sort extends State<Sort> {
-  double _value = 5;
+  double _value = 10;
   int _radioValue1 = 1;
   bool _checkBoxValue = false;
 
+  List<int> array;
+
+  @override
+  void initState() {
+    super.initState();
+    array = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+    generateRandomArray();
+  }
+
+  void generateRandomArray() {
+
+    var range = Random();
+    array = List.generate(10, (_) => range.nextInt(100));
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,7 +59,6 @@ class _Sort extends State<Sort> {
                       style: TextStyle(fontSize: 18.0),
                     ),
                   ),
-
                   Slider(
                     value: _value,
                     min: 1,
@@ -53,7 +68,8 @@ class _Sort extends State<Sort> {
                     inactiveColor: Colors.grey,
                     onChanged: (value) {
                       setState(() {
-                        print(value);
+                        _value = value.toInt().toDouble();
+                        print('Slider value: $_value');
                       });
                     },
                   ),
@@ -108,7 +124,11 @@ class _Sort extends State<Sort> {
                     width: 100.0,
                   ),
                   RaisedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        array.add(100);
+                      });
+                    },
                     child: Text("Sort"),
                     color: Colors.blue,
                   )
@@ -116,7 +136,7 @@ class _Sort extends State<Sort> {
               ),
             ),
             CustomPaint(
-              painter: DrawBars(),
+              painter: DrawBars(array: array),
               child: Container(
                 margin: EdgeInsets.all(8.0),
                 height: MediaQuery.of(context).copyWith().size.height - 150,
@@ -148,22 +168,45 @@ class DrawBars extends CustomPainter {
 
   int barCount;
 
-  DrawBars({this.barCount});
+  List<int> array;
+
+  DrawBars({this.array});
 
   @override
   void paint(Canvas canvas, Size size) {
     final paint = Paint();
+    Offset start, end;
 
     // Draw bottom line
     paint.color = Colors.black;
     var startPoint = Offset(50, size.height - 50);
     var endPoint = Offset(size.width - 50, size.height - 50);
     canvas.drawLine(startPoint, endPoint, paint);
+
+    // Draw sart and end points
+    
+    // Get working area
+    start = Offset(100, size.height - 50);
+    end = Offset(size.width - 100, size.height - 50);
+
+    // Draw circles
+    canvas.drawCircle(start, 5, paint);
+    canvas.drawCircle(end, 5, paint);
+
+    // Draw bars
+    for(int i = 0; i < array.length; i++) {
+      if(i % 2 == 0) {
+        paint.color = Colors.blue;
+      } else {
+        paint.color = Colors.black;
+      }      
+      canvas.drawRect(Rect.fromLTWH(start.dx + (i * 20), start.dy - (array[i]), 20.0, (array[i]).toDouble()), paint);
+    }
+
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
-    // TODO: implement shouldRepaint
-    throw UnimplementedError();
+    return true;
   }
 }
